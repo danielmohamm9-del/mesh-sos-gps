0ed35c437d253396f6afe6fdb1e61bff220a459f
danielmohamm9-del <danielmohamm9@gmail.com>
2026-09-15 23:16:58 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,22 +5,16 @@ plugins {
 
 android {
     namespace = "com.emergency.meshgps"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "com.emergency.meshgps"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
@@ -28,20 +22,14 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
-    buildFeatures {
-        viewBinding = true
-    }
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-
-    // Google Services, Osmdroid, & JSON Parser
-    implementation("com.google.android.gms:play-services-location:21.1.0")
-    implementation("com.google.android.gms:play-services-nearby:19.1.0")
-    implementation("org.osmdroid:osmdroid-android:6.1.18")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+    implementation("com.google.android.gms:play-services-nearby:19.0.0")
+    implementation("org.osmdroid:osmdroid-android:6.1.16")
     implementation("com.google.code.gson:gson:2.10.1")
 }
